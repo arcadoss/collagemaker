4ff2d80ce8aa61cf98290d6f0260dc226d17aef2
Sergey Dovgal <arcadoss-f@ya.ru>
2014-07-03 14:25:45 +0400

    Gson converter replaced with Jackson

--- app/src/main/java/ru/arcadoss/collagemaker/web/InstagramAdapter.java
+++ app/src/main/java/ru/arcadoss/collagemaker/web/InstagramAdapter.java
@@ -1,5 +1,8 @@
 package ru.arcadoss.collagemaker.web;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -25,11 +28,21 @@ public class InstagramAdapter {
 			.create();
 
 
+	private static final JacksonConverter JACKSON_CONVERTER;
+	static {
+		JsonFactory factory = new JsonFactory();
+		ObjectMapper mapper = new ObjectMapper(factory);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+		JACKSON_CONVERTER = new JacksonConverter(mapper, factory);
+	}
+
 	public static final RestAdapter get() {
 		RestAdapter adapter = new RestAdapter.Builder()
 				.setRequestInterceptor(HEADER_INJECTOR)
 				.setEndpoint(Instagram.ENDPOINT)
 				.setLogLevel(RestAdapter.LogLevel.FULL)
+				.setConverter(JACKSON_CONVERTER)
 				.build();
 
 		return adapter;
